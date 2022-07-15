48825d785b08c461fd81d40edb53284102afe7a2
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-07-15 15:36:28 +0300

    finished task

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,9 @@
 const parser = (testUrl) => {
   const url = new URL(testUrl);
 
+  if (!validURL(url)) {
+    throw new Error("Invalid URL");
+  }
   const urlParams = {};
 
   url.searchParams.forEach((value, name) => {
@@ -24,6 +27,7 @@ const parser = (testUrl) => {
           return (urlParams[name] = value);
       }
     }
+
     const arrWithDot = name.split(".");
 
     if (arrWithDot.length === 2) {
@@ -111,148 +115,19 @@ const parser = (testUrl) => {
   return Object.keys(urlParams)[0] ? urlParams : null;
 };
 
+//* validation
+function validURL(str) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return pattern.test(str);
+}
+//* validation
+
 module.exports = parser;
-
-// const parser = (testUrl) => {
-//   console.log(testUrl);
-//   const myUrl = new URL(testUrl);
-
-//   const arr = myUrl.search.replace("?", "").split("&");
-//   console.log(arr);
-//   const obj = arr.reduce((acc, item) => {
-//     let itemArr = item.split("=");
-//     const isDot = itemArr[0].indexOf(".");
-//     const value = itemArr[1];
-
-//     if (isDot === -1) {
-//       const key = itemArr[0];
-
-//       switch (true) {
-//         case !value:
-//           break;
-//         case !isNaN(Number(value)):
-//           acc = {
-//             ...acc,
-//             [key]: Number(value),
-//           };
-//           break;
-//         case value[0] === '"' && value[value.length - 1] === '"':
-//           acc = {
-//             ...acc,
-//             [key]: value.replace(/['"]+/g, ""),
-//           };
-//           break;
-
-//         case value === "true":
-//           acc = {
-//             ...acc,
-//             [key]: true,
-//           };
-//           break;
-//         case value === "false":
-//           acc = {
-//             ...acc,
-//             [key]: false,
-//           };
-//           break;
-//         default:
-//           acc = {
-//             ...acc,
-//             [key]: value,
-//           };
-//           break;
-//       }
-//     }
-
-//     const arrWithDot = itemArr[0].split(".");
-
-//     if (arrWithDot.length === 2) {
-//       const firstKey = arrWithDot[0];
-//       const secondKey = arrWithDot[1];
-
-//       switch (true) {
-//         case !value:
-//           break;
-//         case value[0] === '"' && value[value.length - 1] === '"':
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: value.replace(/['"]+/g, "") },
-//           };
-//           break;
-
-//         case !isNaN(Number(value)):
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: Number(value) },
-//           };
-//           break;
-//         case value === "true":
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: true },
-//           };
-//           break;
-//         case value === "false":
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: false },
-//           };
-//           break;
-
-//         default:
-//           acc = { ...acc, [firstKey]: { [secondKey]: value } };
-//           break;
-//       }
-//     }
-
-//     if (arrWithDot.length === 3) {
-//       const firstKey = arrWithDot[0];
-//       const secondKey = arrWithDot[1];
-//       const thirdKey = arrWithDot[2];
-
-//       switch (true) {
-//         case !value:
-//           break;
-//         case value[0] === '"' && value[value.length - 1] === '"':
-//           acc = {
-//             ...acc,
-//             [firstKey]: {
-//               [secondKey]: { [thirdKey]: value.replace(/['"]+/g, "") },
-//             },
-//           };
-//           break;
-//         case !isNaN(Number(value)):
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: { [thirdKey]: Number(value) } },
-//           };
-//           break;
-//         case value === "true":
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: { [thirdKey]: true } },
-//           };
-//           break;
-//         case value === "false":
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: { [thirdKey]: false } },
-//           };
-//           break;
-//         default:
-//           acc = {
-//             ...acc,
-//             [firstKey]: { [secondKey]: { [thirdKey]: value } },
-//           };
-//           break;
-//       }
-//     }
-
-//     return acc;
-//   }, {});
-
-//   console.log("JSON:", JSON.stringify(obj, null, 2));
-//   return Object.keys(obj)[0] ? obj : null;
-// };
-
-// module.exports = parser;
