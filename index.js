f09ae86891280ae3bde1ccc108997cb4cbd970fa
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-07-18 00:46:06 +0300

    done validation+cheked value

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const parser = require("./src/parserNew");
 
 // return parser(process.argv[2]);
 return parser(
-  "http://myurl.com?foo.bar.www=42&foo.bar.zzz=42&foo.baz=hello&bar.baz=true&baz=11"
+  "http://myurl.com?foo.bar.www=33&foo.bar.zzz=42&foo.baz=hello&bar.baz=true&baz=11"
 );
--- src/parserNew.js
+++ src/parserNew.js
@@ -1,20 +1,32 @@
 const parser = (testUrl) => {
   const indexStartQueryString = testUrl.indexOf("?") + 1;
   const queryString = testUrl.substring(indexStartQueryString);
-  console.log(queryString);
+
+  if (indexStartQueryString === 0 || queryString.length === 0) {
+    return null;
+  }
+  if (queryString.indexOf("?") !== -1) {
+    throw new Error("Sorry, incorrect format: more then one '?' ");
+  }
   const resultObj = {};
   queryString.split("&").forEach((queryParam) => {
     parsQuery(queryParam, resultObj);
   });
-  console.log(resultObj);
+  console.log(JSON.stringify(resultObj, null, 2));
   return resultObj;
 };
 
 function parsQuery(queryParam, resultObj) {
   const keyPair = queryParam.split("=");
+  if (keyPair.length > 2) {
+    throw new Error("Sorry, incorrect format: more then one '=' ");
+  }
   const key = keyPair[0];
   const value = keyPair[1];
-  makeFinalObj(key, value, resultObj);
+  if (value || value.length > 0) {
+    const validateValue = checkValue(value);
+    makeFinalObj(key, validateValue, resultObj);
+  }
 }
 
 function makeFinalObj(key, value, resultObj) {
@@ -28,6 +40,22 @@ function makeFinalObj(key, value, resultObj) {
     const secondObj = key.substring(key.indexOf(".") + 1);
     makeFinalObj(secondObj, value, resultObj[keyFragment[0]]);
   }
+}
+
+function checkValue(value) {
+  if (value[0] === '"' && value[value.length - 1] === '"') {
+    return value.replace(/['"]+/g, "");
+  }
+  if (!isNaN(Number(value))) {
+    return Number(value);
+  }
+  if (value === "true" || value === "false") {
+    return Boolean(value);
+  }
+  if (value.indexOf('"') !== -1) {
+    throw new Error("Sorry, incorrect format: error with brackets ");
+  }
+  return value;
 }
 
 module.exports = parser;
